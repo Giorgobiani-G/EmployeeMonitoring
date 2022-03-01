fd3838263da8a8ccbaa7d2a26838fb59a034b90c
admin <admin@DESKTOP-LILFTDD>
2022-03-01 23:44:12 +0400

    doubleclickproblem

--- EmployeeMonitoring/MainWindow.xaml.cs
+++ EmployeeMonitoring/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+
 
 namespace EmployeeMonitoring
 {
@@ -42,30 +45,89 @@ namespace EmployeeMonitoring
                  
             };
 
-            var gasvlisdro = from db in context.MyProperty
-                             where db.Saxeli == emp.Saxeli
-                             select new{ db.WasvlisDro };
-              
-            
-          int dagvianeba =  DateTime.Compare((DateTime)emp.ShesvlisDro, DateTime.Parse("09:14"));
 
-            if (dagvianeba > 0 && gasvlisdro is null)
+
+            var gasvla = (from db in context.MyProperty
+                             where db.Saxeli == emp.Saxeli &&
+                             db.ShesvlisDro.Value.Year == emp.ShesvlisDro.Value.Year &&
+                             db.ShesvlisDro.Value.Month == emp.ShesvlisDro.Value.Month &&
+                             db.WasvlisDro == null &&
+                             db.GacceniliSaatebi != null
+                          select db).Any();
+
+
+
+            int dagvianeba = DateTime.Compare((DateTime)emp.ShesvlisDro, DateTime.Parse("22:14"));
+
+            //tu daigviana
+
+            if (dagvianeba > 0 && gasvla == false)
             {
-              emp.GacceniliSaatebi =  emp.ShesvlisDro.Value.Subtract(DateTime.Parse("09:00")).TotalHours;
+                var variable = emp.ShesvlisDro.Value;
+                TimeSpan timeSpan = (TimeSpan)(emp.ShesvlisDro - new DateTime(variable.Year, variable.Month, variable.Day, 9, 0, 0));
+                emp.GacceniliSaatebi = timeSpan.TotalHours;
             }
             _ = await context.AddAsync(emp);
             _ = await context.SaveChangesAsync();
         }
 
+
+
+        int count=0;
         private async void Gasvla_Click_1(object sender, RoutedEventArgs e)
         {
+            
+            count++;
+
+            if (count > 1)
+            {
+                Gasvla.Click -= Gasvla_Click_1;
+                System.Timers.Timer timer = new System.Timers.Timer(8000);
+                timer.Enabled = false;
+                timer.AutoReset = false;
+                timer.Elapsed += Timer_Elapsed;
+                timer.Start();
+                //timer.Close();
+              
+                count = 0;
+                return;
+            }
+
+           
+
             EmpModel emp = new EmpModel
             {
                 Saxeli = txtbox.Text,
                 WasvlisDro = DateTime.Now
+
             };
+
+            
+
+            var empfromdatabase = (from db in context.MyProperty
+                                   where db.Saxeli == emp.Saxeli &&
+                                   db.ShesvlisDro.Value.Year == emp.WasvlisDro.Value.Year &&
+                                   db.ShesvlisDro.Value.Month == emp.WasvlisDro.Value.Month &&
+                                    //db.GacceniliSaatebi != null&&
+                                   db.WasvlisDro == null
+                                   select db).ToArray().LastOrDefault();
+
+            if (empfromdatabase != null)
+            {
+                var variabel = empfromdatabase.ShesvlisDro.Value;
+                TimeSpan timeSpan = (TimeSpan)(emp.WasvlisDro - 
+                    new DateTime(variabel.Year, variabel.Month, variabel.Day, variabel.Hour, variabel.Minute, variabel.Second));
+                emp.GacceniliSaatebi = timeSpan.TotalHours;
+            }
+           
             _ = await context.AddAsync(emp);
             _ = await context.SaveChangesAsync();
+           
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Gasvla.Click += Gasvla_Click_1;
         }
     }
 }
